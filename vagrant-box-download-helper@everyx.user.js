5f2870789f0e706534e7bb9a10c888502be70b0d
everyx <lunt.luo@gmail.com>
2019-07-10 14:33:59 +0800

    Compatible with new websites app.vagrantup.com

--- vagrant-box-download-helper@everyx.user.js
+++ vagrant-box-download-helper@everyx.user.js
@@ -1,41 +1,37 @@
 // ==UserScript==
-// @id             vagrant-box-download-helper@everyx
-// @name           Vagrant Box Download Helper
-// @version        1.0
-// @namespace      http://userscript.everyx.in/vagrant-box-download-helper
-// @author         everyx
-// @description    Add a download button in atlas.hashicorp.com.
-// @include        https://app.vagrantup.com/*/boxes/*
-// @run-at         document-end
+// @name         Vagrant Box Download Helper
+// @namespace    http://userscript.everyx.in
+// @version      2.0
+// @description  Add a download button in app.vagrantup.com.
+// @author       everyx
+// @match        https://app.vagrantup.com/*/boxes/*
+// @grant        none
+// @downloadURL  https://github.com/everyx/vagrant-box-download-helper-everyx.user.js/raw/master/vagrant-box-download-helper@everyx.user.js
+// @updateURL    https://github.com/everyx/vagrant-box-download-helper-everyx.user.js/raw/master/vagrant-box-download-helper@everyx.user.js
+// @run-at       document-end
 // ==/UserScript==
 
-main();
+(function () {
+    var versionList = document.querySelectorAll(".page-header h3 a");
+    var panelList = document.querySelectorAll(".panel");
 
-function main() {
-  var versionList = document.querySelectorAll(".page-header h3 a");
-  var panelList = document.querySelectorAll(".panel");
+    for (var i = 0; i < versionList.length; i++) {
+        var providerElements = panelList[i].querySelectorAll('.list-group-item');
 
-  for (var i = 0; i < versionList.length; i++) {
-    addDownloadButton(versionList[i].href, panelList[i]);
-  }
-}
+        for (var j = 0; j < providerElements.length; j++) {
+            var element = providerElements[j]
 
-function addDownloadButton(versionLink, panelElement) {
-  var providerElements = panelElement.querySelectorAll('.list-group-item');
+            var downloadButton = document.createElement('a');
+            downloadButton.className = 'pull-right glyphicon glyphicon-download';
+            downloadButton.title = 'download';
 
-  for (var i=0; i<providerElements.length; i++) {
-    var element = providerElements[i]
-    console.log(element);
-    var downloadButton = document.createElement('a');
-    downloadButton.className ='pull-right glyphicon glyphicon-download';
-    downloadButton.title = 'download';
-  
-    var ignoreText = element.querySelector('small').innerHTML;
-    var provider = element.innerText.replace(ignoreText, '').trim();
-    downloadButton.href = versionLink + '/providers/' + provider + '.box';
-    console.log(downloadButton.href)
-  
-    element.querySelector('h4').appendChild(downloadButton);
-    
-  }
-}
+            var ignoreText = element.querySelector('small').innerText;
+            var provider = element.innerText.replace(ignoreText, '').trim();
+            downloadButton.href = versionList[i].href + '/providers/' + provider + '.box';
+
+
+            element.querySelector('h4').appendChild(downloadButton);
+
+        }
+    }
+})();
